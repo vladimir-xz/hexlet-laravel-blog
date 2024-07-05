b11ca8c240fa1304dc170fd7b7fef8df2ba49b8f
vladimir-xz <vladimir156xz@gmail.com>
2024-07-05 19:53:47 +0200

    first controller for welcome page and about

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,7 @@
 <?php
 
+use App\Http\Controllers\PageController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::get('/about', function () {
-    return view('about');
-});
+Route::get('', [PageController::class, 'welcome']);
+Route::get('about', [PageController::class, 'about']);
